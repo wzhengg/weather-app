58a7efa844503ce0ee71d0008abbee2dda4decb1
William Zheng <williamzheng2001@gmail.com>
2022-07-04 11:47:21 -0400

    Remove HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,8 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-    }),
-  ],
   devServer: {
     static: "./dist",
   },
