3554bb1baa80fd7ff9a5fa737e3ec197cbe71149
William Zheng <williamzheng2001@gmail.com>
2022-07-08 13:04:58 -0400

    Update data when units change

--- src/weatherData.js
+++ src/weatherData.js
@@ -2,24 +2,31 @@ import PubSub from "pubsub-js";
 import {
   FETCH_WEATHER_ERROR,
   SEARCHED_LOCATION,
+  UNITS_CHANGED,
   WEATHER_DATA_UPDATED,
 } from "./pubsubTopics";
+import { units } from "./units";
 import { getWeather, processData } from "./weatherAPI";
 
+let storedLocation = "San Francisco";
 let data;
 
 export default function initWeatherData() {
   PubSub.subscribe(SEARCHED_LOCATION, updateData);
+  PubSub.subscribe(UNITS_CHANGED, updateData);
+  updateData(null, storedLocation);
 }
 
 async function updateData(_topic, location) {
   try {
-    const rawData = await getWeather(location);
+    if (location === undefined) {
+      location = storedLocation;
+    }
+    const rawData = await getWeather(location, units());
     data = processData(rawData);
     PubSub.publish(WEATHER_DATA_UPDATED, data);
+    storedLocation = location;
   } catch (error) {
     PubSub.publish(FETCH_WEATHER_ERROR);
   }
 }
-
-updateData(null, "San Francisco");
