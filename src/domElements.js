f22a9cb398117135ef3bc7a591e54a4ee0b4f9c1
William Zheng <williamzheng2001@gmail.com>
2022-07-07 20:03:16 -0400

    Get reference to weather elements

--- src/domElements.js
+++ src/domElements.js
@@ -1,2 +1,12 @@
 export const searchBar = document.getElementById("search-bar");
 export const searchBtn = document.querySelector(".search-btn");
+
+export const location = document.querySelector(".location");
+export const mainTemp = document.querySelector(".temp");
+export const feelsLike = document.querySelector(".feels-like");
+export const descIcon = document.querySelector(".desc-icon");
+export const descText = document.querySelector(".desc-text");
+export const highTemp = document.querySelector(".high-temp");
+export const windSpeed = document.querySelector(".wind-speed");
+export const lowTemp = document.querySelector(".low-temp");
+export const humidityPercent = document.querySelector(".humidity-percent");
