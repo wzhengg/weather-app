b9f1ef3134cf864b62a1407ab9efee634abb071e
William Zheng <williamzheng2001@gmail.com>
2022-07-08 13:05:16 -0400

    Update UI when units change

--- src/weatherUI.js
+++ src/weatherUI.js
@@ -1,5 +1,5 @@
 import PubSub from "pubsub-js";
-import { WEATHER_DATA_UPDATED } from "./pubsubTopics";
+import { UNITS_CHANGED, WEATHER_DATA_UPDATED } from "./pubsubTopics";
 import {
   location,
   mainTemp,
@@ -11,20 +11,35 @@ import {
   lowTemp,
   humidityPercent,
 } from "./domElements";
+import { units } from "./units";
 
-const degreeSign = "\xB0";
+const DEGREE_SIGN = "\xB0";
+let tempUnit;
+let speedUnit;
 
 export default function initWeatherUI() {
+  updateUnits();
+  PubSub.subscribe(UNITS_CHANGED, updateUnits);
   PubSub.subscribe(WEATHER_DATA_UPDATED, displayWeather);
 }
 
 function displayWeather(_topic, data) {
   location.textContent = `${data.city}, ${data.country}`;
-  mainTemp.textContent = `${data.temp} ${degreeSign}F`;
-  feelsLike.textContent = `Feels like ${data.feelsLike} ${degreeSign}F`;
+  mainTemp.textContent = `${data.temp} ${tempUnit}`;
+  feelsLike.textContent = `Feels like ${data.feelsLike} ${tempUnit}`;
   descText.textContent = `${data.desc}`;
-  highTemp.textContent = `${data.high} ${degreeSign}F`;
-  lowTemp.textContent = `${data.low} ${degreeSign}F`;
-  windSpeed.textContent = `${data.wind}mph`;
+  highTemp.textContent = `${data.high} ${tempUnit}`;
+  lowTemp.textContent = `${data.low} ${tempUnit}`;
+  windSpeed.textContent = `${data.wind} ${speedUnit}`;
   humidityPercent.textContent = `${data.humidity}%`;
+}
+
+function updateUnits() {
+  if (units() === "imperial") {
+    tempUnit = `${DEGREE_SIGN}F`;
+    speedUnit = "mph";
+  } else {
+    tempUnit = `${DEGREE_SIGN}C`;
+    speedUnit = "km/h";
+  }
 }
