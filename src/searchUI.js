2d57cc573893b2b5e4b03ddb06ffa2c404c2de73
William Zheng <williamzheng2001@gmail.com>
2022-07-08 21:13:51 -0400

    Display search error

--- src/searchUI.js
+++ src/searchUI.js
@@ -1,6 +1,6 @@
 import PubSub from "pubsub-js";
-import { SEARCHED_LOCATION } from "./pubsubTopics";
-import { searchBar, searchBtn } from "./domElements";
+import { FETCH_WEATHER_ERROR, SEARCHED_LOCATION } from "./pubsubTopics";
+import { searchBar, searchBtn, errorModal, closeModalBtn } from "./domElements";
 
 function emptySearch() {
   return searchBar.value.trim().length === 0;
@@ -16,6 +16,12 @@ function search() {
   clearSearch();
 }
 
+function displayError() {
+  errorModal.showModal();
+}
+
 export default function initSearch() {
+  PubSub.subscribe(FETCH_WEATHER_ERROR, displayError);
+  closeModalBtn.addEventListener("click", () => errorModal.close());
   searchBtn.addEventListener("click", search);
 }
